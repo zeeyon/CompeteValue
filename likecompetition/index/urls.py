816500b33ff1bec5a34649c6c94cf013c2e378b7
Hyunjun Lee <bessapple14@naver.com>
2020-07-14 17:08:20 +0900

    댓글 기능 작업 중
    post.title 사라지는 버그

--- likecompetition/index/urls.py
+++ likecompetition/index/urls.py
@@ -4,10 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), # index
     path('<int:page>', views.index, name='index'), # index pagination
-    path('create/', views.create, name='create'), # create.html
     path('post/<int:post_id>', views.post, name='post'), # post.html
-    path('update/', views.update, name='update'), # update.html
-    path('postcreate/', views.postcreate, name='postcreate'), # create 함수
-    path('postupdate/<int:post_id>', views.postupdate, name='postupdate'), # update 함수
-    path('postdelete/<int:post_id>', views.postdelete, name='postdelete'), # delete 함수
+    path('create/', views.create_post, name='create_post'), # create_post 함수
+    path('update/<int:post_id>', views.update_post, name='update_post'), # update_post 함수
+    path('delete/<int:post_id>', views.delete_post, name='delete_post'), # delete_post 함수
 ]
