5c05960d83dad86b7cac6476cee8fd1694f6b530
Hyunjun Lee <bessapple14@naver.com>
2020-07-15 17:21:47 +0900

    change models.py, admin.py
    change models.py, admin.py

--- likecompetition/index/admin.py
+++ likecompetition/index/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Scrap
 
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Scrap)
