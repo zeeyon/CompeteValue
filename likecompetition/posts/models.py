bf79565152a4532dfd1de4c26b7f00dd0746e5f1
zeeyon <qor3127@naver.com>
2020-07-21 02:14:27 +0900

    add posts models (crud 0.6v)

--- likecompetition/posts/models.py
+++ likecompetition/posts/models.py
@@ -5,11 +5,37 @@ class Post(models.Model):
     title = models.CharField(max_length=50, null=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+    GENDER_CHOICES = {
+        ('male','Male'),
+        ('female','Female'),
+    }
+    gender = models.CharField(max_length=50, choices=GENDER_CHOICES)
+    age = models.IntegerField(null=True)
+    CITY_CHOICES = {
+        ('Seoul','서울특별시'),
+        ('Incheon','인천광역시'),
+    }
+    # AREA_CHOICES = [{
+    #     ('Seoul', (
+    #             ('gangnam-gu','강남구'),
+    #             ('gangdong-gu','강동구'),
+    #         )
+    #     )
+    # },{
+    #     ('Incheon', (
+    #             ('gyeyang-gu','계양구'),
+    #             ('nam-gu','남구'),
+    #         )
+    #     )
+    # }]
+    city = models.CharField(max_length=50, choices=CITY_CHOICES)
+    # area = models.CharField(max_length=50, choices=AREA_CHOICES[AREA_CHOICES.index(city)]) 
     content = models.TextField(default='')
     view_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
