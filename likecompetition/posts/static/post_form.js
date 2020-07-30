0101354df91323d73c8037f00b1b721d1fca0a2a
gjals <hhuurr1004@naver.com>
2020-07-30 16:02:15 +0900

    base, post 수정
    base 타이틀 바 수정, 글쓰기 수정

---
+++ likecompetition/posts/static/post_form.js
@@ -0,0 +1,20 @@
+
+$("#id_city").change(function () {
+	const url = $("#areaForm").attr("data-areas-url");
+	const cityId = $(this).val();
+
+	$.ajax({
+		url: url,
+		data: {
+			'city_id': cityId
+		},
+		success: function (data) {
+			$("#id_area").html(data);
+		}
+	});
+});
+
+
+$(document).ready(function(){
+	$('body').css("backgroundColor", "gainsboro");
+});
