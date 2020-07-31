c3e01dcb9ea7471a0ee8cca595134bcf6dad74ab
Hyunjun Lee <bessapple14@naver.com>
2020-07-31 17:59:12 +0900

    table을 div로 변경
    table을 div로 변경

--- likecompetition/posts/static/index.js
+++ likecompetition/posts/static/index.js
@@ -4,50 +4,59 @@ $(document).ready(function(){
 	})
 });
 
-var ages=[
-	{name:'age-10', text:'10대'},
-	{name:'age-20', text:'20대'},
-	{name:'age-30', text:'30대'}
-];
+var all=null;
+var check_lists=[];
 
-var genders=[
-	{name:'gender-male', text:'남성'},
-	{name:'gender-female', text:'여성'}
-]
-
-var areas=[
-	{name:'area-seoul', text:'서울'},
-	{name:'area-busan', text:'부산'},
-	{name:'area-daegu', text:'대구'},
-	{name:'area-gwanju', text:'광주'},
-	{name:'area-incheon', text:'인천'},
-	{name:'area-daejeon', text:'대전'},
-	{name:'area-ulsan', text:'울산'},
-	{name:'area-gyeonggi', text:'경기'},
-	{name:'area-gangwon', text:'강원'},
-	{name:'area-chungcheongbuk', text:'충북'},
-	{name:'area-chungcheongnam', text:'충남'},
-	{name:'area-jeollabuk', text:'전북'},
-	{name:'area-jeollanam', text:'전남'},
-	{name:'area-gyeongsangbuk', text:'경북'},
-	{name:'area-gyenogsangnam', text:'경남'},
-	{name:'area-seoul', text:'세종'},
-	{name:'area-jeju', text:'제주'}
-]
-
-var fields=[
-	{name:'field-web', text:'웹'},
-	{name:'field-mobile', text:'모바일'},
-	{name:'field-research', text:'연구'},
-	{name:'field-ai', text:'인공지능'},
-	{name:'field-bigdata', text:'빅데이터'},
-	{name:'field-game', text:'게임'},
-	{name:'field-embedded/web', text:'임베디드'},
-	{name:'field-security', text:'보안'},
-	{name:'field-etc', text:'기타'}
-]
-
-var all= ages.concat(genders, areas, fields);
+/**
+ * 체크박스
+ */
+ var check_box=new Vue({
+	delimiters: ['[[', ']]'],
+	el:'#check-box',
+	data:{
+		qwerty:[
+			{name_kor:'나이', name_eng:'age', elems:[
+				{name:'age10', text:'10대'},
+				{name:'age20', text:'20대'},
+				{name:'age30', text:'30대'}
+			]},
+			{name_kor:'성별', name_eng:'gender', elems:[
+				{name:'male', text:'남성'},
+				{name:'female', text:'여성'}
+			]},
+			{name_kor:'지역', name_eng:'area', elems:[
+				{name:'seoul', text:'서울'},
+				{name:'busan', text:'부산'},
+				{name:'daegu', text:'대구'},
+				{name:'incheon', text:'인천'},
+				{name:'gwangju', text:'광주'},
+				{name:'daejeon', text:'대전'},
+				{name:'ulsan', text:'울산'},
+				{name:'sejong', text:'세종'},
+				{name:'gyeonggi', text:'경기'},
+				{name:'gangwon', text:'강원'},
+				{name:'chungbuk', text:'충북'},
+				{name:'chungnam', text:'충남'},
+				{name:'jeonbuk', text:'전북'},
+				{name:'jeonnam', text:'전남'},
+				{name:'gyeongbuk', text:'경북'},
+				{name:'gyenognam', text:'경남'},
+				{name:'jeju', text:'제주'}
+			]},
+			{name_kor:'분야', name_eng:'field', elems:[
+				{name:'web', text:'웹'},
+				{name:'mobile', text:'모바일'},
+				{name:'research', text:'연구'},
+				{name:'ai', text:'인공지능'},
+				{name:'bigdata', text:'빅데이터'},
+				{name:'game', text:'게임'},
+				{name:'embedded', text:'임베디드'},
+				{name:'security', text:'보안'},
+				{name:'etc', text:'기타'}
+			]}
+		]
+	}
+ })
 
 function check_lists_reload(){
 	for(var i=0; i<check_lists.length; i++)
@@ -64,16 +73,6 @@ function check_lists_reload(){
 		check_lists.push(all[checked_list[j]].text);  
 }
 
-var check_lists=[];
-
-var check_list=new Vue({
-	delimiters: ['[[', ']]'],
-	el:'#check-box-add',
-	data:{
-		check_list:check_lists
-	}
-})
-
 function check_box_remove(el){
 	var k=0;
 	for(var k=0; k<all.length; k++)
@@ -87,53 +86,3 @@ function check_box_remove(el){
 	});
 	check_lists_reload();
 }
-
-/**
- * 체크박스
- */
-
-var check_age=new Vue({
-	delimiters: ['[[', ']]'],
-	el:'#check-box-age',
-	data:{
-		age:ages
-	}
-})
-
-var check_gender=new Vue({
-	delimiters: ['[[', ']]'],
-	el:'#check-box-gender',
-	data:{
-		gender:genders
-	}
-})
-
-var check_area=new Vue({
-	delimiters: ['[[', ']]'],
-	el:'#check-box-area',
-	data:{
-		area:areas
-	}
-})
-
-var check_field=new Vue({
-	delimiters: ['[[', ']]'],
-	el:'#check-box-field',
-	data:{
-		field:fields
-	}
-})
-
-/* 해시태그
-$(function() {
-	var availableTags = [
-	“바나나”,
-	“사과”,
-	“오렌지”,
-	“수박”
-	]; //자동으로 완성될 단어들을 입력해줍니다
-	$( “#tags” ).autocomplete({
-	source: availableTags //자동완성 될 소스는 위에 선언한 availableTags이다.
-	});
-	
-}) */
