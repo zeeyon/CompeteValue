59660adeeea4510d57b95d7dc2ce20b914b091c5
Hyunjun Lee <bessapple14@naver.com>
2020-08-03 13:15:59 +0900

    change check-box to filter-box
    change check-box to filter-box

--- likecompetition/likecompetition/static/base.js
+++ likecompetition/likecompetition/static/base.js
@@ -1,25 +1,4 @@
-var sideBars=[
-	{text:'홈', link:"/"},
-	{text:'글쓰기', link:'/posts/new'} 
-];
-
 function toggleMenu(){
 	var sideBar=$("#side-bar");
 	sideBar.toggleClass('open');
 }
-
-var side_bar=new Vue({
-	delimiters: ['[[', ']]'],  
-	el:'#side-bar',
-	data:{
-		sideBar:sideBars
-	}
-})
-
-var header_menu=new Vue({
-	delimiters: ['[[', ']]'],
-	el:'#header-menu',
-	data:{
-		sideBar:sideBars
-	}
-})
--- likecompetition/posts/static/index.js
+++ likecompetition/posts/static/index.js
@@ -1,30 +1,18 @@
-$(document).ready(function(){
-	$("input[type=checkbox]").click(function(){
-		check_lists_reload();
-	})
-});
-
-var all=null;
-var check_lists=[];
-
-/**
- * 체크박스
- */
- var check_box=new Vue({
+var filter_box = new Vue({
 	delimiters: ['[[', ']]'],
-	el:'#check-box',
-	data:{
-		qwerty:[
-			{name_kor:'나이', name_eng:'age', elems:[
+	el: '#filter-box',
+	data: {
+		filters: [
+			{name_kor:'나이', name_eng:'age', options:[
 				{name:'age10', text:'10대'},
 				{name:'age20', text:'20대'},
 				{name:'age30', text:'30대'}
 			]},
-			{name_kor:'성별', name_eng:'gender', elems:[
+			{name_kor:'성별', name_eng:'gender', options:[
 				{name:'male', text:'남성'},
 				{name:'female', text:'여성'}
 			]},
-			{name_kor:'지역', name_eng:'area', elems:[
+			{name_kor:'지역', name_eng:'area', options:[
 				{name:'seoul', text:'서울'},
 				{name:'busan', text:'부산'},
 				{name:'daegu', text:'대구'},
@@ -43,7 +31,7 @@ var check_lists=[];
 				{name:'gyenognam', text:'경남'},
 				{name:'jeju', text:'제주'}
 			]},
-			{name_kor:'분야', name_eng:'field', elems:[
+			{name_kor:'분야', name_eng:'field', options:[
 				{name:'web', text:'웹'},
 				{name:'mobile', text:'모바일'},
 				{name:'research', text:'연구'},
@@ -54,35 +42,12 @@ var check_lists=[];
 				{name:'security', text:'보안'},
 				{name:'etc', text:'기타'}
 			]}
-		]
+		],
+		check_list: []
+	},
+	methods: {
+		remove_option: function(option) {
+			this.check_list.splice(this.check_list.indexOf(option), 1);
+		}
 	}
- })
-
-function check_lists_reload(){
-	for(var i=0; i<check_lists.length; i++)
-	$("#check-box-add-li").remove();
-
-	var checked_list=[];
-	var i=0;
-	$("input[type=checkbox]").each(function(){ 
-		if($(this).is(":checked"))  
-			checked_list.push(i);
-		i++;
-	});
-	for(var j=0; j<checked_list.length; j++)
-		check_lists.push(all[checked_list[j]].text);  
-}
-
-function check_box_remove(el){
-	var k=0;
-	for(var k=0; k<all.length; k++)
-		if(all[k].text.concat("X")==el.closest("li").textContent)
-			break;
-
-	$("input[type=checkbox]").each(function(){ 
-		if(k==0)  
-			$(this).prop("checked", false);
-		k--;
-	});
-	check_lists_reload();
-}
+})
