65e788fe03f83af1e1ed7b0114dd3e94e0d57d3a
gjals <hhuurr1004@naver.com>
2020-09-09 01:38:29 +0900

    post_detail 팝업
    post 북마크, 삭제 comment 삭제 안되서 수정해야함
    뒤로가기 수정해야함

--- likecompetition/posts/forms.py
+++ likecompetition/posts/forms.py
@@ -26,3 +26,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'placeholder': 'Add a comment...'}),
+        }
